e3c1627e48a182abc6248fa5b4519c2f9e126f53
s.mostafa.rahmani@gmail.com <s.mostafa.rahmani@gmail.com>
2019-05-02 10:49:29 +0430

    It's being completed :)

--- estexare.py
+++ estexare.py
@@ -8,8 +8,6 @@ import sqlite3
 from sqlite3 import Error
 
 db = "./estexare.db"
-print(f"DB file: {db}\n")
-
 try:
     conn = sqlite3.connect(db)
 except Error as e:
@@ -19,22 +17,23 @@ except Error as e:
 conn.row_factory = sqlite3.Row
 cur = conn.cursor()
 
-
 MAX_ESTEXARE_NO = 181
-estexare_no = random.randint(1,MAX_ESTEXARE_NO)
+estexare_no = random.randint(1, MAX_ESTEXARE_NO)
 select_estexare_query = f"""
     select * from estexare
     where rowid = {estexare_no}
     """
-print(select_estexare_query)
-
-cur.execute(select_estexare_query) #, tuple(str(estexare_no)))
+cur.execute(select_estexare_query)  # , tuple(str(estexare_no)))
 
 estexare_row = cur.fetchone()
-print(estexare_row)
-for i in estexare_row:
-    print(i)
-    
+print(estexare_row['fi_result'])
+
+if '-c' in sys.argv:
+    print('yes')
+
+# for i in estexare_row:
+#     print(i)
+
 # print(f"{estexare_row[0]}")
 # print('')
 
