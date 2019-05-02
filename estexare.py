b861a89a16663394f75574431085983be9be6d60
s.mostafa.rahmani@gmail.com <s.mostafa.rahmani@gmail.com>
2019-05-02 15:19:30 +0430

    Now it works in a loop so user can do estexare multiple times.

--- estexare.py
+++ estexare.py
@@ -19,33 +19,42 @@ cur = conn.cursor()
 
 # Number of records of db.
 MAX_ESTEXARE_NO = 181
-estexare_no = random.randint(1, MAX_ESTEXARE_NO)
-select_estexare_query = f"""
-    select * from estexare
-    where rowid = ?
-    """
-cur.execute(select_estexare_query, (estexare_no,))
 
-estexare_row = cur.fetchone()
+again = True
+while again:
+    estexare_no = random.randint(1, MAX_ESTEXARE_NO)
+    select_estexare_query = f"""
+        select * from estexare
+        where rowid = ?
+        """
+    cur.execute(select_estexare_query, (estexare_no,))
 
-lang = 'fi'
+    estexare_row = cur.fetchone()
 
-if '-l' in sys.argv:
-    try:
-        a_lang = sys.argv[sys.argv.index('-l') + 1]
-        if a_lang in ['fi', 'fa', 'en']:
-            lang = a_lang
-        else:
-            print('The language is not in deinfed languages. Finglish is default.')
-    except IndexError as x:
-        print('No language is declared.')
+    lang = 'fi'
 
-print(estexare_row[lang + '_result'])
-if '-f' in sys.argv:
-    print(f'{estexare_row[lang + "_comment"]}\n')
-    print(
-        f"Sure: {estexare_row['sure']} ({estexare_row['sure_no']}),", end=" ")
-    print(f"Aye: {estexare_row['aye_no']}")
-    print(estexare_row['aye'])
-elif '-c' in sys.argv:
-    print(f'{estexare_row[lang + "_comment"]}')
+    if '-l' in sys.argv:
+        try:
+            a_lang = sys.argv[sys.argv.index('-l') + 1]
+            if a_lang in ['fi', 'fa', 'en']:
+                lang = a_lang
+            else:
+                print('The language is not in deinfed languages. Finglish is default.')
+        except IndexError as x:
+            print('No language is declared.')
+
+    print(estexare_row[lang + '_result'])
+    if '-f' in sys.argv:
+        print(f'{estexare_row[lang + "_comment"]}\n')
+        print(
+            f"Sure: {estexare_row['sure']} ({estexare_row['sure_no']}),", end=" ")
+        print(f"Aye: {estexare_row['aye_no']}")
+        print(estexare_row['aye'])
+    elif '-c' in sys.argv:
+        print(f'{estexare_row[lang + "_comment"]}')
+
+    again_input = input('\nAgain [y/N]: ')
+    again_input = again_input.lstrip().lower()
+    again = len(again_input) > 0 and again_input[0] == 'y'
+
+print('bye')
